0002475de960734e5ffa01fd47352f430e30f846
Muaaz Bhyat <muu794@gmail.com>
2024-05-20 05:08:02 +0200

    updated resources

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import * as THREE from 'three';
 import { TIFFLoader } from 'three/examples/jsm/loaders/TIFFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 import EarthDay from './resources/EarthDay.jpg';
 import EarthNight from './resources/EarthNight.jpg';
-import EarthNormal from './resources/EarthNormalMap.tif';
-import EarthSpecular from './resources/EarthSpecMap.tif';
+import EarthBump from './resources/EarthBumpMap.jpg';
+import EarthSpecular from './resources/EarthSpecMap.jpg';
 import EarthClouds from './resources/Clouds.jpg';
+
+import MoonMap from './resources/MoonMap.tif';
+import MoonBump from './resources/MoonBumpMap.jpg';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -14,42 +19,83 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const tLoader = new TIFFLoader();
 const loader = new THREE.TextureLoader();
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(0, 0, 1);
-scene.add(light);
+const sun = new THREE.DirectionalLight(0xffffff, 1);
+sun.position.set(300, 0, -300);
+sun.castShadow = true;
+scene.add(sun);
 
-const earthGeometry = new THREE.SphereGeometry(1, 32, 32);
+const earthGeometry = new THREE.SphereGeometry(1, 300, 300);
+const earthTexture = loader.load(EarthDay);
+const earthBump = loader.load(EarthBump);
+const earthSpecular = loader.load(EarthSpecular);
+const earthNight = loader.load(EarthNight);
 const earthMaterial = new THREE.MeshPhongMaterial({
-  map: loader.load(EarthDay),
-  normalMap: tLoader.load(EarthNormal),
-  specularMap: tLoader.load(EarthSpecular),
-  specular: new THREE.Color('grey'),
-  emissiveMap: loader.load(EarthNight),
-  emissiveIntensity: 0.1,
-
+  map: earthTexture,
+  bumpMap: earthBump,
+  bumpScale: 0.05,
+  specularMap: earthSpecular,
+  specular: new THREE.Color(0x262626),
+  shininess: 100,
+  emissiveMap: earthNight,
+  emissive: new THREE.Color(0x666666),
+  emissiveIntensity: 0.3,
 });
 const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+earth.castShadow = true;
+earth.receiveShadow = true;
+earth.rotateZ(23.5/180*Math.PI);
 scene.add(earth);
 
-const cloudsGeometry = new THREE.SphereGeometry(1.01, 32, 32);
+const cloudsGeometry = new THREE.SphereGeometry(1.007, 300, 300);
 const cloudsMaterial = new THREE.MeshPhongMaterial({
-  map: loader.load(EarthClouds),
+  alphaMap: loader.load(EarthClouds),
   transparent: true,
   opacity: 0.8,
   depthWrite: false,
 });
 camera.position.z = 5;
+const clouds = new THREE.Mesh(cloudsGeometry, cloudsMaterial);
+clouds.castShadow = true;
+scene.add(clouds);
+
+const moonGeometry = new THREE.SphereGeometry(0.27, 300, 300);
+const moonTexture = tLoader.load(MoonMap);
+const moonBump = loader.load(MoonBump);
+const moonMaterial = new THREE.MeshPhongMaterial({
+  map: moonTexture,
+  bumpMap: moonBump,
+  bumpScale: 1,
+});
+const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+moon.position.set(5, 0, 0);
+moon.castShadow = true;
+moon.receiveShadow = true;
+scene.add(moon);
+
+const orbit = () => {
+  moon.position.x = 5 * Math.cos(Date.now() * 0.0001);
+  moon.position.z = 5 * Math.sin(Date.now() * 0.0001);
+}
 
 function animate() {
   requestAnimationFrame(animate);
-
+  earth.rotation.y += 0.001;
+  clouds.rotation.y += 0.0015;
+  moon.rotation.y += 0.001;
+  orbit();
+  controls.update();
   renderer.render(scene, camera);
 }
 
